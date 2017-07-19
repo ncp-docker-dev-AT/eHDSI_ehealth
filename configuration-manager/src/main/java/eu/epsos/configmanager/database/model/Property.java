cec9741ad98cbd869746069db38944b04cf8341f
SUBIGER Jerome <jerome.subiger@ext.ec.europa.ec>
2017-07-19 19:29:36 +0200

    EHNCP-1270 Validating and bug fixing cross dependencies between Openncp components

--- configuration-manager/src/main/java/eu/epsos/configmanager/database/model/Property.java
+++ configuration-manager/src/main/java/eu/epsos/configmanager/database/model/Property.java
@@ -3,8 +3,10 @@ package eu.epsos.configmanager.database.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "property")
 public class Property {
 
     @Id
