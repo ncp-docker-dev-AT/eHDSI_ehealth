d59bd0496979e1ef6de1f62943c363e6cf5d9c0f
SUBIGER Jerome <jerome.subiger@ext.ec.europa.ec>
2017-07-24 12:51:43 +0200

    EHNCP-1275 Logging configuration EHNCP-1274 Code formatting

--- openncp-gateway/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/smpeditor/entities/SMPFieldProperties.java
+++ openncp-gateway/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/smpeditor/entities/SMPFieldProperties.java
@@ -1,31 +1,32 @@
 package eu.europa.ec.sante.ehdsi.openncp.gateway.smpeditor.entities;
 
 /**
- *
  * @author Inês Garganta
  */
 
 public class SMPFieldProperties {
 
-  private boolean enable;
-  private boolean mandatory;
+    private boolean enable;
+    private boolean mandatory;
 
-  public SMPFieldProperties() {
-    super();
-  }
-  
-  public boolean isEnable() {
-    return enable;
-  }
-  public void setEnable(boolean enable) {
-    this.enable = enable;
-  }
-  
-  public boolean isMandatory() {
-    return mandatory;
-  }
-  public void setMandatory(boolean mandatory) {
-    this.mandatory = mandatory;
-  }
+    public SMPFieldProperties() {
+        super();
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public boolean isMandatory() {
+        return mandatory;
+    }
+
+    public void setMandatory(boolean mandatory) {
+        this.mandatory = mandatory;
+    }
 
 }
