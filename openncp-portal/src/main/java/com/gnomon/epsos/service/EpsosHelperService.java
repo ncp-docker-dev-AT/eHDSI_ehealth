0273de29e1eb4f5dd33810405f8629b9baa9ea62
SUBIGER Jerome <jerome.subiger@ext.ec.europa.ec>
2017-09-08 13:26:28 +0200

    EHNCP-1274 Code formatting

--- openncp-portal/src/main/java/com/gnomon/epsos/service/EpsosHelperService.java
+++ openncp-portal/src/main/java/com/gnomon/epsos/service/EpsosHelperService.java
@@ -43,7 +43,6 @@ import org.htmlcleaner.PrettyXmlSerializer;
 import org.htmlcleaner.TagNode;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
 import org.opensaml.Configuration;
 import org.opensaml.DefaultBootstrap;
 import org.opensaml.common.SAMLObjectBuilder;
@@ -304,14 +303,14 @@ public class EpsosHelperService {
 
         List<EDDetail> edDetails = new ArrayList<>();
         for (ViewResult dispensedLine : dispensedLines) {
-            ViewResult d_line = dispensedLine;
+
             EDDetail ed = new EDDetail();
-            ed.setRelativePrescriptionLineId(d_line.getField1().toString());
-            ed.setDispensedQuantity(d_line.getField7().toString());
-            ed.setMedicineFormCode(d_line.getField5().toString());
-            ed.setMedicineCommercialName(d_line.getField2().toString());
+            ed.setRelativePrescriptionLineId(dispensedLine.getField1().toString());
+            ed.setDispensedQuantity(dispensedLine.getField7().toString());
+            ed.setMedicineFormCode(dispensedLine.getField5().toString());
+            ed.setMedicineCommercialName(dispensedLine.getField2().toString());
             // Setting the substitution indicator
-            ed.setSubstituted((Boolean) d_line.getField3());
+            ed.setSubstituted((Boolean) dispensedLine.getField3());
             edDetails.add(ed);
         }
         cda.setEDDetail(edDetails);
