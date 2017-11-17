c02c8e55dd18e90a8405fb314d9eb770ffe31637
SUBIGER Jerome <jerome.subiger@ext.ec.europa.ec>
2017-11-17 20:55:34 +0100

    EHNCP-1274 Code formatting

--- openatna/anom/src/main/java/org/openhealthtools/openatna/anom/AtnaObject.java
+++ openatna/anom/src/main/java/org/openhealthtools/openatna/anom/AtnaObject.java
@@ -1,23 +1,3 @@
-/**
- * Copyright (c) 2009-2011 University of Cardiff and others
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * permissions and limitations under the License.
- * <p>
- * Contributors:
- * University of Cardiff - initial API and implementation
- * -
- */
-
 package org.openhealthtools.openatna.anom;
 
 import java.io.Serializable;
@@ -29,8 +9,6 @@ import java.util.List;
  *
  * @author Andrew Harrison
  * @version $Revision:$
- * @created Sep 5, 2009: 2:37:18 PM
- * @date $Date:$ modified by $Author:$
  */
 public class AtnaObject implements Serializable {
 
@@ -180,23 +158,22 @@ public class AtnaObject implements Serializable {
 
     @Override
     public String toString() {
-        return new StringBuilder().append("[")
-                .append(getClass().getName())
-                .append(" object id=")
-                .append(getObjectId())
-                .append(" object name=")
-                .append(getObjectName())
-                .append(" object type=")
-                .append(getObjectTypeCode())
-                .append(" object type role=")
-                .append(getObjectTypeCodeRole())
-                .append(" object id type=")
-                .append(getObjectIdTypeCode())
-                .append(" object sensitivity=")
-                .append(getObjectSensitivity())
-                .append(" object detail types=")
-                .append(getObjectDetailTypes())
-                .append("]")
-                .toString();
+        return "[" +
+                getClass().getName() +
+                " object id=" +
+                getObjectId() +
+                " object name=" +
+                getObjectName() +
+                " object type=" +
+                getObjectTypeCode() +
+                " object type role=" +
+                getObjectTypeCodeRole() +
+                " object id type=" +
+                getObjectIdTypeCode() +
+                " object sensitivity=" +
+                getObjectSensitivity() +
+                " object detail types=" +
+                getObjectDetailTypes() +
+                "]";
     }
 }
