c0bdcb896e3a86920b901c463c4fbdf7ace92738
SUBIGER Jerome <jerome.subiger@ext.ec.europa.ec>
2017-12-07 23:19:30 +0100

    EHNCP-1414 OpenNCP Common Components re-factoring

---
+++ openncp-common-components/openncp-configuration-utility/src/main/java/eu/europa/ec/sante/ehdsi/PropertyRepository.java
@@ -0,0 +1,6 @@
+package eu.europa.ec.sante.ehdsi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyRepository extends JpaRepository<Property, String> {
+}
