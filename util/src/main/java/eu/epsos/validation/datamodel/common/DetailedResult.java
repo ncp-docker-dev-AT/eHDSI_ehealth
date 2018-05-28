507969e9dab2166b845ad4f890d39824e029ee3e
subigje <jerome.subiger@ext.ec.europa.ec>
2018-05-28 22:11:23 +0200

    EHNCP-1447 Code Quality - Fixing Code Smells

--- util/src/main/java/eu/epsos/validation/datamodel/common/DetailedResult.java
+++ util/src/main/java/eu/epsos/validation/datamodel/common/DetailedResult.java
@@ -1,29 +1,9 @@
-/*
- * This file is part of epSOS OpenNCP implementation
- * Copyright (C) 2013 SPMS (Serviços Partilhados do Ministério da Saúde - Portugal)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact email: epsos@iuz.pt
- */
 package eu.epsos.validation.datamodel.common;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
  * @author Marcelo Fonseca <marcelo.fonseca@iuz.pt>
  */
 @XmlRootElement
@@ -38,6 +18,7 @@ public class DetailedResult {
     private SchematronValidation schematronValidation;
 
     /* GETTERS AND SETTERS */
+
     /**
      * @return the documentValidXsd
      */
@@ -127,6 +108,6 @@ public class DetailedResult {
     public void setSchematronValidation(SchematronValidation schematronValidation) {
         this.schematronValidation = schematronValidation;
     }
-    
-    
+
+
 }
