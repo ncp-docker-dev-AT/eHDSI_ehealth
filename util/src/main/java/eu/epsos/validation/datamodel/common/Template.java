f714a665a24cd3d78bf5f711722b6234987ab251
subigje <jerome.subiger@ext.ec.europa.ec>
2018-05-28 22:16:25 +0200

    EHNCP-1447 Code Quality - Fixing Code Smells

--- util/src/main/java/eu/epsos/validation/datamodel/common/Template.java
+++ util/src/main/java/eu/epsos/validation/datamodel/common/Template.java
@@ -1,29 +1,9 @@
- /*
-  *  This file is part of epSOS OpenNCP implementation
-  *  Copyright (C) 2013 SPMS (Serviços Partilhados do Ministério da Saúde - Portugal)
-  *
-  *  This program is free software: you can redistribute it and/or modify
-  *  it under the terms of the GNU General Public License as published by
-  *  the Free Software Foundation, either version 3 of the License, or
-  *  (at your option) any later version.
-  *
-  *  This program is distributed in the hope that it will be useful,
-  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *  GNU General Public License for more details.
-  *
-  *  You should have received a copy of the GNU General Public License
-  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  *
-  *  Contact email: epsos@iuz.pt
-  */
-
 package eu.epsos.validation.datamodel.common;
 
-import java.util.List;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import java.util.List;
 
 /**
  * This class represents a Template object.
