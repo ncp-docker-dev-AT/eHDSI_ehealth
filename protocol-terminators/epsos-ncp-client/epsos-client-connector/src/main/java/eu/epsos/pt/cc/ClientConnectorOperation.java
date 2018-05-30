99a7243ea7e9cef204f1c65e0fd55c4375e9a22f
subigje <jerome.subiger@ext.ec.europa.ec>
2018-05-30 18:27:10 +0200

    EHNCP-1556 Define OpenNCP method name as a Constant

---
+++ protocol-terminators/epsos-ncp-client/epsos-client-connector/src/main/java/eu/epsos/pt/cc/ClientConnectorOperation.java
@@ -0,0 +1,13 @@
+package eu.epsos.pt.cc;
+
+public class ClientConnectorOperation {
+
+    public static final String SERVICE_QUERY_PATIENT = "queryPatient";
+    public static final String SERVICE_QUERY_DOCUMENTS = "queryDocuments";
+    public static final String SERVICE_RETRIEVE_DOCUMENT = "retrieveDocument";
+    public static final String SERVICE_SUBMIT_DOCUMENT = "submitDocument";
+    public static final String SERVICE_TEST_SAY_HELLO = "sayHello";
+
+    private ClientConnectorOperation() {
+    }
+}
