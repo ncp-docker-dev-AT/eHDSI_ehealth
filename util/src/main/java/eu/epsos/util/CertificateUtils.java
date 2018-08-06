effb1fe7b1d671137d740208c316c6d13642ff3b
subigje <jerome.subiger@ext.ec.europa.ec>
2018-08-06 15:54:59 +0200

    EHNCP-1447 Code Quality - Fixing Code Smells

--- util/src/main/java/eu/epsos/util/CertificateUtils.java
+++ util/src/main/java/eu/epsos/util/CertificateUtils.java
@@ -1,22 +1,3 @@
-/*
- * This file is part of epSOS OpenNCP implementation
- * Copyright (C) 2015  GNOMON Informatics)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact email: k.karkaletsis@gnomon.com.gr
- */
 package eu.epsos.util;
 
 import epsos.ccd.netsmart.securitymanager.key.KeyStoreManager;
@@ -30,7 +11,6 @@ import tr.com.srdc.epsos.util.http.HTTPUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.cert.X509Certificate;
@@ -54,7 +34,7 @@ public class CertificateUtils {
         return HTTPUtil.getServerCertificate(endpoint);
     }
 
-    public static X509Certificate getClientCertificate() throws IOException, KeyStoreException {
+    public static X509Certificate getClientCertificate() throws KeyStoreException {
 
         X509Certificate cert = null;
 
