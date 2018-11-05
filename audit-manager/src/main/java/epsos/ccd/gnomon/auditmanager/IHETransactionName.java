ce08d88386f8596e6801defae6c77ab2162510b4
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2018-11-05 18:09:31 +0100

    EHNCP-1638 Java Source Code formatting

--- audit-manager/src/main/java/epsos/ccd/gnomon/auditmanager/IHETransactionName.java
+++ audit-manager/src/main/java/epsos/ccd/gnomon/auditmanager/IHETransactionName.java
@@ -1,42 +1,24 @@
-/***Licensed to the Apache Software Foundation (ASF) under one
- *or more contributor license agreements.  See the NOTICE file
- *distributed with this work for additional information
- *regarding copyright ownership.  The ASF licenses this file
- *to you under the Apache License, Version 2.0 (the
- *"License"); you may not use this file except in compliance
- *with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- *Unless required by applicable law or agreed to in writing,
- *software distributed under the License is distributed on an
- *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *KIND, either express or implied.  See the License for the
- *specific language governing permissions and limitations
- *under the License.
- **//*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package epsos.ccd.gnomon.auditmanager;
 
 /**
  * Enumeration for populating the EventType of the AuditMessage
- * One of the availbale epsos event ids
- * epsos-11: epsosIdentificationService
- * epsos-21: epsosPatientService
- * epsos-31: epsosOrderService
- * epsos-41: epsodDispensationServuceInitialize
- * epsos-42: epsodDispensationServuceDiscard
- * epsos-51: epsodConsentServicePut
- * epsos-52: epsodConsentServiceDiscard
- * epsos-53: epsodConsentServicePin
- * epsos-91: epsosHcpAuthentication
- * epsos-92: epsosTRCAssertion
- * epsos-93: epsosNCPTrustedServiceList
- * epsos-94: epsosPivotTranslation
- * epsos-cf: epsosCommunicationFailure
+ * One of the available eHDSI event ids:
+ * <p>
+ * epsos-11:    epsosIdentificationService
+ * epsos-21:    epsosPatientService
+ * epsos-31:    epsosOrderService
+ * epsos-41:    epsosDispensationServuceInitialize
+ * epsos-42:    epsosDispensationServuceDiscard
+ * epsos-51:    epsosConsentServicePut
+ * epsos-52:    epsosConsentServiceDiscard
+ * epsos-53:    epsosConsentServicePin
+ * epsos-91:    epsosHcpAuthentication
+ * epsos-92:    epsosTRCAssertion
+ * epsos-93:    epsosNCPTrustedServiceList
+ * epsos-94:    epsosPivotTranslation
+ * epsos-cf:    epsosCommunicationFailure
+ * ehealth-193: SMP::Query
+ * ehealth-194: SMP::Push
  *
  * @author Kostas Karkaletsis
  * @author Organization: Gnomon
@@ -44,19 +26,6 @@ package epsos.ccd.gnomon.auditmanager;
  * @version 1.0, 2010, 30 Jun
  */
 public enum IHETransactionName {
-//    epsosIdentificationServiceFindIdentityByTraits("epsosIdentityService::FindIdentityByTraits"),
-//    epsosPatientServiceList("epsosPatientService::List"),
-//    epsosOrderServiceList("epsosOrderService::List"),
-//    epsosDispensationServiceInitialize("epsosDispensationService::Initialize"),
-//    epsosDispensationServiceDiscard("epsosDispensationService::Discard"),
-//    epsosConsentServicePut("epsosConsentService::Put"),
-//    epsosConsentServiceDiscard("epsosConsentService::Discard"),
-//    epsosConsentServicePin("epsosConsentService::PIN"),
-//    epsosHcpAuthentication("identityProvider::HcpAuthentication"),
-//    epsosTRCAssertion("ncp::TrcAssertion"),
-//    epsosNCPTrustedServiceList("ncpConfigurationManager::ImportNSL"),
-//    epsosPivotTranslation("ncpTransformationMgr::Translate"),
-//    epsosCommunicationFailure("epsosCommunicationFailure");
 
     epsosIdentificationServiceFindIdentityByTraits("XCPD::CrossGatewayPatientDiscovery"),
     epsosPatientServiceList("XCA::CrossGatewayQuery"),
