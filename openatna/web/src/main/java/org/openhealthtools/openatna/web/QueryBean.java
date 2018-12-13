9d6ee594532b6f1c6623cf4541e94ecd50c853ba
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2018-12-13 16:20:16 +0100

    EHNCP-1658 Minor code refactoring on OpenNCP component

--- openatna/web/src/main/java/org/openhealthtools/openatna/web/QueryBean.java
+++ openatna/web/src/main/java/org/openhealthtools/openatna/web/QueryBean.java
@@ -1,21 +1,21 @@
 /**
- *  Copyright (c) 2009-2010 University of Cardiff and others
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- *  implied. See the License for the specific language governing
- *  permissions and limitations under the License.
- *
- *  Contributors:
- *    University of Cardiff - initial API and implementation
- *    -
+ * Copyright (c) 2009-2010 University of Cardiff and others
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * <p>
+ * Contributors:
+ * University of Cardiff - initial API and implementation
+ * -
  */
 
 package org.openhealthtools.openatna.web;
@@ -25,7 +25,6 @@ package org.openhealthtools.openatna.web;
  * @version 1.0.0
  * @date Jan 19, 2010: 11:48:17 AM
  */
-
 public class QueryBean {
 
     private String sourceId;
@@ -237,15 +236,14 @@ public class QueryBean {
         return other;
     }
 
-	@Override
-	public String toString() {
-		return "QueryBean [sourceId=" + sourceId + ", objectId=" + objectId + ", participantId=" + participantId
-				+ ", eventOutcome=" + eventOutcome + ", eventIdCode=" + eventIdCode + ", eventTypeCode="
-				+ eventTypeCode + ", eventAction=" + eventAction + ", eventTime=" + eventTime + ", startDate="
-				+ startDate + ", startHour=" + startHour + ", startMin=" + startMin + ", participantTypeCode="
-				+ participantTypeCode + ", sourceTypeCode=" + sourceTypeCode + ", objectTypeCode=" + objectTypeCode
-				+ ", endDate=" + endDate + ", endHour=" + endHour + ", endMin=" + endMin + ", sourceAddress="
-				+ sourceAddress + ", maxResults=" + maxResults + ", startOffset=" + startOffset + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "QueryBean [sourceId=" + sourceId + ", objectId=" + objectId + ", participantId=" + participantId
+                + ", eventOutcome=" + eventOutcome + ", eventIdCode=" + eventIdCode + ", eventTypeCode="
+                + eventTypeCode + ", eventAction=" + eventAction + ", eventTime=" + eventTime + ", startDate="
+                + startDate + ", startHour=" + startHour + ", startMin=" + startMin + ", participantTypeCode="
+                + participantTypeCode + ", sourceTypeCode=" + sourceTypeCode + ", objectTypeCode=" + objectTypeCode
+                + ", endDate=" + endDate + ", endHour=" + endHour + ", endMin=" + endMin + ", sourceAddress="
+                + sourceAddress + ", maxResults=" + maxResults + ", startOffset=" + startOffset + "]";
+    }
 }
