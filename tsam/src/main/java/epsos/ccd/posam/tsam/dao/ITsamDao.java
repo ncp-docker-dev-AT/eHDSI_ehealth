0fd47ae74d2eb5f929c6bb77d68f9d0e65af1c76
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2019-03-11 14:57:04 +0100

    EHNCP-1698 Minor code refactoring and BugFix on OpenNCP components

--- tsam/src/main/java/epsos/ccd/posam/tsam/dao/ITsamDao.java
+++ tsam/src/main/java/epsos/ccd/posam/tsam/dao/ITsamDao.java
@@ -1,14 +1,13 @@
 package epsos.ccd.posam.tsam.dao;
 
-import java.util.List;
-
 import epsos.ccd.posam.tsam.exception.TSAMException;
 import epsos.ccd.posam.tsam.model.CodeSystem;
 import epsos.ccd.posam.tsam.model.CodeSystemConcept;
 import epsos.ccd.posam.tsam.model.CodeSystemVersion;
 import epsos.ccd.posam.tsam.model.Designation;
-import epsos.ccd.posam.tsam.model.ValueSetVersion;
 import epsos.ccd.posam.tsam.response.RetrievedConcept;
+
+import java.util.List;
 
 /**
  * Data Acces Object layer
@@ -17,44 +16,42 @@ import epsos.ccd.posam.tsam.response.RetrievedConcept;
  * @author Organization: Posam
  * @author mail:roman.repiscak@posam.sk
  * @version 1.0, 2010, 11 August
- *
  */
 public interface ITsamDao {
 
     /**
-     * Check if association between CodeSystemConcept and ValueSet with
-     * valueSetOid exist. If valueSetVersion is providet, checks if
+     * Check if association between CodeSystemConcept and ValueSet with valueSetOid exist.
+     * If valueSetVersion is provided, checks if
      *
      * @param concept
      * @param valueSetOid
      * @param valueSetVersion - name of ValueSetVersion
-     * @return true if assocation exist else false
+     * @return true if association exist else false
      */
-    public boolean valueSetMatches(CodeSystemConcept concept, String valueSetOid, String valueSetVersion);
+    boolean valueSetMatches(CodeSystemConcept concept, String valueSetOid, String valueSetVersion);
 
     /**
-     * Retrieve Designation for Target CodeSystemConcept and selected language
+     * Retrieve Designation for Target CodeSystemConcept and selected language.
      *
      * @param target
      * @param lang
      * @return
      * @throws TSAMException if designation is not found
      */
-    public List<Designation> getDesignation(CodeSystemConcept target, String lang) throws TSAMException;
+    List<Designation> getDesignation(CodeSystemConcept target, String lang) throws TSAMException;
 
     /**
-     * Retrieve Target CodeSystemConcept from Transcoding association by Source
-     * Concept
+     * Retrieve Target CodeSystemConcept from Transcoding association by Source Concept.
      *
      * @param sourceConcept
      * @return
      * @throws TSAMException if target concept is not found
      */
-    public CodeSystemConcept getTargetConcept(CodeSystemConcept sourceConcept) throws TSAMException;
+    CodeSystemConcept getTargetConcept(CodeSystemConcept sourceConcept) throws TSAMException;
 
     /**
-     * Retrieve CodeSystemConcept by Code and CodeSystemVersion. Method looks
-     * for concept int provided CodeSystemVersion and all its previous versions
+     * Retrieve CodeSystemConcept by Code and CodeSystemVersion. Method looks for concept int provided CodeSystemVersion
+     * and all its previous versions.
      *
      * @param session
      * @param code
@@ -62,12 +59,11 @@ public interface ITsamDao {
      * @return
      * @throws TSAMException if concept is not found
      */
-    public CodeSystemConcept getConcept(String code, CodeSystemVersion codeSystemVersion) throws TSAMException;
+    CodeSystemConcept getConcept(String code, CodeSystemVersion codeSystemVersion) throws TSAMException;
 
     /**
-     * Retrieve CodeSystemversion by LocalName and parent CodeSystem. If no
-     * version is provided (version is null), method looks for versions of
-     * CodeSystem with status "current".
+     * Retrieve CodeSystemversion by LocalName and parent CodeSystem. If no version is provided (version is null),
+     * method looks for versions of CodeSystem with status "current".
      *
      * @param session
      * @param version
@@ -75,7 +71,7 @@ public interface ITsamDao {
      * @return
      * @throws TSAMException if version is not found
      */
-    public CodeSystemVersion getVersion(String version, CodeSystem system) throws TSAMException;
+    CodeSystemVersion getVersion(String version, CodeSystem system) throws TSAMException;
 
     /**
      * Retrieve CodeSystem by OID
@@ -85,33 +81,30 @@ public interface ITsamDao {
      * @return CodeSystem
      * @throws TSAMException if CodeSystem is not found
      */
-    public CodeSystem getCodeSystem(String oid) throws TSAMException;
+    CodeSystem getCodeSystem(String oid) throws TSAMException;
 
     /**
-     * Method to retrieve all concepts and their current designations for
-     * selected ValueSet ana language. If language is null, default (en)
-     * language is used
+     * Method to retrieve all concepts and their current designations for selected ValueSet ana language.
+     * If language is null, default (en) language is used.
      *
      * @param valueSetOid
      * @param valueSetVersionName
      * @param language
      * @return
      */
-    public List<RetrievedConcept> getConcepts(String valueSetOid, String valueSetVersionName, String language);
+    List<RetrievedConcept> getConcepts(String valueSetOid, String valueSetVersionName, String language);
 
     /**
-     *
      * @param target
      * @return
      * @throws TSAMException
      */
-    public List<Designation> getSourceDesignation(CodeSystemConcept target) throws TSAMException;
+    List<Designation> getSourceDesignation(CodeSystemConcept target) throws TSAMException;
 
     /**
      * This method will return all available languages in the LTR.
      *
-     * @return the list of LTR available languages, as a String list, containing
-     * the language codes;
+     * @return the list of LTR available languages, as a String list, containing the language codes;
      */
-    public List<String> getLtrLanguages();
+    List<String> getLtrLanguages();
 }
