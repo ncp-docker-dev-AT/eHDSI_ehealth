83fcb4e0373c94952f25d048785b2c1c961dc88d
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2019-03-25 16:56:23 +0100

    EHNCP-1734 Fixing connectivity issue - module openncp-discovery-client

--- openncp-common-components/openncp-discovery-client/src/main/java/eu/europa/ec/sante/ehdsi/DynamicDiscoveryClientApplication.java
+++ openncp-common-components/openncp-discovery-client/src/main/java/eu/europa/ec/sante/ehdsi/DynamicDiscoveryClientApplication.java
@@ -5,22 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.StopWatch;
 
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
-@EnableConfigurationProperties
-@EntityScan(basePackages = {"eu.europa.ec.sante.ehdsi"})
 public class DynamicDiscoveryClientApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DynamicDiscoveryClientApplication.class);
-
-    public DynamicDiscoveryClientApplication() {
-    }
+    private static final String PARTICIPANT_IDENTIFIER = "urn:ehealth:eu:ncp-idp";
 
     public static void main(String[] args) {
 
@@ -34,12 +29,14 @@ public class DynamicDiscoveryClientApplication {
         //  b-2c1a0fa2bc1b37789bc426e278b235a0
         //  b-2c1a0fa2bc1b37789bc426e278b235a0.ehealth-participantid-qns.ehealth.testa.eu: Name or service not known
         //  lyndpiwxe5x3jwund7pixgpqohifdeygdj6pzobcz2f66kcecvxa
-        String PARTICIPANT_IDENTIFIER = "urn:ehealth:mt:ncp-idp";
+
         try {
             LOGGER.info("NAPTR Hash: '{}'", HashUtil.getSHA256HashBase32(PARTICIPANT_IDENTIFIER));
             LOGGER.info("CNAME Hash: '{}'", StringUtils.lowerCase("b-" + HashUtil.getMD5Hash(PARTICIPANT_IDENTIFIER)));
         } catch (NoSuchAlgorithmException e) {
             LOGGER.error("NoSuchAlgorithmException: '{}'", e.getMessage(), e);
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("UnsupportedEncodingException: '{}'", e.getMessage(), e);
         }
         try {
             StopWatch stopWatch = new StopWatch();
