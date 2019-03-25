75cb11a807e229f62efc93622e6ac7b686361bae
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2019-03-25 19:11:44 +0100

    EHNCP-1698 Minor code refactoring and BugFix on OpenNCP components

--- protocol-terminators/epsos-ncp-server/epsos-ncpa-interface/src/main/java/eu/epsos/protocolterminators/ws/server/xca/XCAServiceInterface.java
+++ protocol-terminators/epsos-ncp-server/epsos-ncpa-interface/src/main/java/eu/epsos/protocolterminators/ws/server/xca/XCAServiceInterface.java
@@ -1,29 +1,3 @@
-/*
- * Combined interface for Patient Summary and ePrescription XCA Service implementation.
- *
- * Patient summary part by SRDC
- * Copyright (C) 2011, 2012 SRDC Yazilim Arastirma ve Gelistirme ve Danismanlik Tic. Ltd. Sti. <epsos@srdc.com.tr>
- *
- * This file is part of SRDC epSOS NCP.
- *
- * SRDC epSOS NCP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SRDC epSOS NCP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SRDC epSOS NCP. If not, see <http://www.gnu.org/licenses/>.
- *
- * ePrescription part by Kela (The Social Insurance Institution of Finland)
- * GNU General Public License v3
- *
- * Corrections/updates by other epSOS project participants
- */
 package eu.epsos.protocolterminators.ws.server.xca;
 
 import epsos.ccd.gnomon.auditmanager.EventLog;
@@ -36,20 +10,20 @@ import org.apache.axiom.soap.SOAPHeader;
 public interface XCAServiceInterface {
 
     /**
-     * @param pRPA_IN201305UV02
-     * @param sh
+     * @param request
+     * @param soapHeader
      * @param eventLog
      * @return
      * @throws Exception
      */
-    AdhocQueryResponse queryDocument(AdhocQueryRequest pRPA_IN201305UV02, SOAPHeader sh, EventLog eventLog) throws Exception;
+    AdhocQueryResponse queryDocument(AdhocQueryRequest request, SOAPHeader soapHeader, EventLog eventLog) throws Exception;
 
     /**
-     * @param pRPA_IN201305UV02
-     * @param sh
+     * @param request
+     * @param soapHeader
      * @param eventLog
      * @param response
      * @throws Exception
      */
-    void retrieveDocument(RetrieveDocumentSetRequestType pRPA_IN201305UV02, SOAPHeader sh, EventLog eventLog, OMElement response) throws Exception;
+    void retrieveDocument(RetrieveDocumentSetRequestType request, SOAPHeader soapHeader, EventLog eventLog, OMElement response) throws Exception;
 }
--- protocol-terminators/epsos-ncp-server/epsos-ncpa-interface/src/main/java/eu/epsos/protocolterminators/ws/server/xcpd/XCPDServiceInterface.java
+++ protocol-terminators/epsos-ncp-server/epsos-ncpa-interface/src/main/java/eu/epsos/protocolterminators/ws/server/xcpd/XCPDServiceInterface.java
@@ -7,5 +7,12 @@ import org.hl7.v3.PRPAIN201306UV02;
 
 public interface XCPDServiceInterface {
 
-    PRPAIN201306UV02 queryPatient(PRPAIN201305UV02 pRPA_IN201305UV02, SOAPHeader sh, EventLog eventLog) throws Exception;
+    /**
+     * @param request
+     * @param soapHeader
+     * @param eventLog
+     * @return
+     * @throws Exception
+     */
+    PRPAIN201306UV02 queryPatient(PRPAIN201305UV02 request, SOAPHeader soapHeader, EventLog eventLog) throws Exception;
 }
