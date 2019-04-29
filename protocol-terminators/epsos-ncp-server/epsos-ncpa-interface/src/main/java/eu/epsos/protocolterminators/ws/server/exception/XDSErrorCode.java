2a865a7754b2bd6dab767cec3189101c9552e853
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2019-04-29 17:26:18 +0200

    EHNCP-1377 eD Req (XDR) error doesn't return MessageID in Location

--- protocol-terminators/epsos-ncp-server/epsos-ncpa-interface/src/main/java/eu/epsos/protocolterminators/ws/server/exception/XDSErrorCode.java
+++ protocol-terminators/epsos-ncp-server/epsos-ncpa-interface/src/main/java/eu/epsos/protocolterminators/ws/server/exception/XDSErrorCode.java
@@ -7,8 +7,12 @@ package eu.epsos.protocolterminators.ws.server.exception;
 public enum XDSErrorCode implements ErrorCode {
 
     //TODO: List of Error Codes shall be completed according the specification.
+    NO_MATCHING_PRESCRIPTION("4105", "No Match"),
     INVALID_DISPENSE("4106", "Invalid Dispensation"),
-    NO_MATCHING_PRESCRIPTION("4105", "No Match");
+    ORIGINAL_DATA_MISSING("4107", "Original data missing"),
+    PIVOT_DATA_MISSING("4108", "Pivot data missing"),
+    WEAK_AUTHENTICATION("4702", "Weak Authentication"),
+    NO_SIGNATURE("4704", "No Signature");
 
     private final String code;
     private final String message;
