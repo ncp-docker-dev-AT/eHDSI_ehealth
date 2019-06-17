986f611fb17e646990313680fd44843197f4b0f8
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2019-06-17 19:42:37 +0200

    EHNCP-1495 Refactoring of eADC according updated specifications

--- eadc/src/main/java/eu/epsos/pt/eadc/util/EadcUtil.java
+++ eadc/src/main/java/eu/epsos/pt/eadc/util/EadcUtil.java
@@ -1,22 +1,3 @@
-/*
- * This file is part of epSOS OpenNCP implementation
- * Copyright (C) 2013  SPMS (Serviços Partilhados do Ministério da Saúde - Portugal)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact email: epsos@iuz.pt
- */
 package eu.epsos.pt.eadc.util;
 
 import com.spirit.epsos.cc.adc.EadcEntry;
@@ -99,9 +80,9 @@ public class EadcUtil {
         eadcReceiver = new EadcReceiverImpl();
         eadcReceiver.process(eadcEntry);
 
-        LOGGER.info("[EADC] Transaction Processing Finished...");
         watch.stop();
         LOGGER.debug("[EADC] method invokeEADC executed in: '{}ms'", watch.getTime());
+        LOGGER.info("[EADC] Transaction Processing Finished...");
     }
 
     /**
