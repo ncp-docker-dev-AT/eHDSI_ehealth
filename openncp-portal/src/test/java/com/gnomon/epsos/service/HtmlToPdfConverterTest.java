3754a727549017ef8068f7742414f759ab13fa42
ghysmat <mathias.ghys@ext.ec.europa.eu>
2019-12-13 09:52:34 +0100

    [EHNCP-1867] Improve and fix bugs related to PDF generation

--- openncp-portal/src/test/java/com/gnomon/epsos/service/HtmlToPdfConverterTest.java
+++ openncp-portal/src/test/java/com/gnomon/epsos/service/HtmlToPdfConverterTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,15 +13,15 @@ import java.nio.file.Paths;
 public class HtmlToPdfConverterTest {
 
     @Test
-    public void testConvertion() throws IOException {
-        String html = readFile(new File("openncp-portal/src/test/resources/ps_friendly.html"));
+    public void testConversion() throws IOException {
+        String html = readFile(new File("src/test/resources/GR2FR_01059902062.html"));
         ByteArrayOutputStream baos = HtmlToPdfConverter.createPdf(html);
         Assert.assertNotNull(baos);
         Assert.assertTrue(baos.toByteArray().length > 0);
-//        String filename = "C:\\LocalData\\Share\\openncp-configuration\\EpsosRepository\\out\\output.pdf";
-//        FileOutputStream output = new FileOutputStream(filename);
-//        output.write(baos.toByteArray());
-//        output.close();
+        /*String filename = "C:\\LocalData\\Share\\openncp-configuration\\EpsosRepository\\out\\output.pdf";
+        FileOutputStream output = new FileOutputStream(filename);
+        output.write(baos.toByteArray());
+        output.close();*/
     }
 
     private String readFile(File file) throws IOException {
