8b89a284dc50136ffc4a375aedf56a4dce9402e2
Jerome Subiger <jerome.subiger@ext.ec.europa.ec>
2020-04-24 10:34:20 +0200

    EHNCP-1920 OpenNCP minor improvements

--- configuration-manager/src/main/java/eu/europa/ec/sante/ehdsi/openncp/configmanager/ConfigurationManagerException.java
+++ configuration-manager/src/main/java/eu/europa/ec/sante/ehdsi/openncp/configmanager/ConfigurationManagerException.java
@@ -2,6 +2,8 @@ package eu.europa.ec.sante.ehdsi.openncp.configmanager;
 
 public class ConfigurationManagerException extends RuntimeException {
 
+    private static final long serialVersionUID = -8449667586219303875L;
+
     public ConfigurationManagerException(String message) {
         super(message);
     }
