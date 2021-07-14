73026d133aab16158eb3397842d834f8301ab36a
Jerome SUBIGER <jerome.subiger@ext.ec.europa.eu>
2021-07-14 16:38:46 +0200

    EHNCP-2140 BugFix OpenATNA viewer - JDK 11 and Spring Web 5.3.X

--- openatna/web/src/main/java/org/openhealthtools/openatna/web/ErrorController.java
+++ openatna/web/src/main/java/org/openhealthtools/openatna/web/ErrorController.java
@@ -1,5 +1,6 @@
 package org.openhealthtools.openatna.web;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openhealthtools.openatna.audit.persistence.AtnaPersistenceException;
 import org.openhealthtools.openatna.audit.persistence.dao.ErrorDao;
 import org.openhealthtools.openatna.audit.persistence.model.ErrorEntity;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -35,24 +34,24 @@ public class ErrorController {
     @GetMapping(value = "/errors")
     public String errors(ModelMap model) {
 
-        QueryBean bean = new QueryBean();
-        model.addAttribute("queryBean", bean);
+        var queryBean = new QueryBean();
+        model.addAttribute("queryBean", queryBean);
         return "errorForm";
     }
 
     @PostMapping(value = "/errors")
-    public ModelAndView errors(HttpServletRequest request, HttpServletResponse response, QueryBean queryBean) throws Exception {
+    public ModelAndView errors(QueryBean queryBean) throws AtnaPersistenceException {
 
-        ModelMap modelMap = new ModelMap();
-        List<? extends ErrorEntity> ents = query(queryBean);
-        if (ents != null) {
+        var modelMap = new ModelMap();
+        List<? extends ErrorEntity> errorEntities = query(queryBean);
+        if (errorEntities != null) {
             List<StringifiedError> list = new ArrayList<>();
-            for (ErrorEntity ent : ents) {
+            for (ErrorEntity ent : errorEntities) {
                 list.add(new StringifiedError(ent));
             }
             modelMap.addAttribute("errors", list);
         } else {
-            modelMap.addAttribute("errors", new ArrayList());
+            modelMap.addAttribute("errors", new ArrayList<StringifiedError>());
         }
         modelMap.addAttribute("queryBean", queryBean);
         return new ModelAndView("errorForm", modelMap);
@@ -60,26 +59,24 @@ public class ErrorController {
 
     private List<? extends ErrorEntity> query(QueryBean bean) throws AtnaPersistenceException {
 
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        var simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         List<? extends ErrorEntity> errorEntities = null;
         Date startDate = null;
-        if (bean.getStartDate() != null && bean.getStartDate().length() > 0) {
+        if (StringUtils.isNotBlank(bean.getStartDate())) {
             String date = bean.getStartDate();
             try {
-                startDate = format.parse(date + " " + bean.getStartHour() + ":" + bean.getStartMin());
+                startDate = simpleDateFormat.parse(date + " " + bean.getStartHour() + ":" + bean.getStartMin());
             } catch (ParseException e) {
                 logger.error("ParseException: '{}'", e.getMessage(), e);
             }
         }
         Date endDate = null;
-        if (bean.getEndDate() != null && bean.getEndDate().length() > 0) {
+        if (StringUtils.isNotBlank(bean.getEndDate())) {
             String date = bean.getEndDate();
             try {
-                endDate = format.parse(date + " " + bean.getEndHour() + ":" + bean.getEndMin());
-                if (startDate != null) {
-                    if (endDate.before(startDate)) {
-                        endDate = null;
-                    }
+                endDate = simpleDateFormat.parse(date + " " + bean.getEndHour() + ":" + bean.getEndMin());
+                if (startDate != null && endDate.before(startDate)) {
+                    endDate = null;
                 }
             } catch (ParseException e) {
                 logger.error("ParseException: '{}'", e.getMessage(), e);
--- openatna/web/src/main/profiled-resource/web/redirect.jsp
+++ openatna/web/src/main/profiled-resource/web/redirect.jsp
@@ -1,21 +1,19 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
-<html>
-
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="css/style.css" title="Default">
     <link rel="stylesheet" type="text/css" href="css/smoothness/jquery-ui-1.7.2.custom.css" title="Default">
-    <title>OpenATNA</title>
+    <title>eHDSI OpenATNA</title>
 </head>
 <body>
 
 <div class="main">
-    <h1>OpenATNA Audit Message and Error Viewer</h1>
-
-    <p>Click <a href="query.html">here</a> to view messages</p>
-
-    <p>Click <a href="errors.html">here</a> to view errors</p>
+    <h1>eHDSI OpenATNA Audit Message and Error Viewer</h1>
+    <p>Click <a href="<c:url value="query"/>" here</a> to view messages</p>
+    <p>Click <a href="<c:url value="errors"/>">here</a> to view errors</p>
 </div>
-
-
+</body>
 </html>
