027fe96d0ae4352e8739c952c294c26fde54ecf4
Samuel Morello <samuel.morello@ouvanous.com>
2022-01-19 16:26:22 +0100

    EHNCP-2263

--- openncp-gateway/openncp-gateway-frontend/src/views/atna-viewer/AuditMessages.vue
+++ openncp-gateway/openncp-gateway-frontend/src/views/atna-viewer/AuditMessages.vue
@@ -8,20 +8,11 @@
       </v-breadcrumbs>
     </div>
     <v-card>
-      <v-card-title>
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
       <v-card-text>
         <v-data-table
           :headers="headers"
           :items="messages"
+          :items-per-page="itemsPerPage"
           :options.sync="options"
           :server-items-length="totalMessages"
           :loading="loading"
@@ -49,18 +40,26 @@ export default {
   data () {
     return {
       headers: [
-        { text: '#', value: 'id' },
-        { text: 'Event Action Code', value: 'eventActionCode' },
-        { text: 'Event ID Code', value: 'eventId' },
-        { text: 'Event Type Codes', value: 'eventTypes' },
-        { text: 'Event Date Time', value: 'eventDateTime' },
-        { text: 'Event Outcome Indicator', value: 'eventOutcomeIndicator' },
+        { text: '#', value: 'id', sortable: false },
+        {
+          text: 'Event Action Code',
+          value: 'eventActionCode',
+          sortable: false
+        },
+        { text: 'Event ID Code', value: 'eventId', sortable: false },
+        { text: 'Event Type Codes', value: 'eventTypes', sortable: false },
+        { text: 'Event Date Time', value: 'eventDateTime', sortable: false },
+        {
+          text: 'Event Outcome Indicator',
+          value: 'eventOutcomeIndicator',
+          sortable: false
+        },
         { value: 'actions', sortable: false }
       ],
       messages: [],
+      itemsPerPage: 10,
       totalMessages: 0,
-      options: { page: 0 },
-      search: '',
+      options: { page: 1 },
       loading: true,
       items: [
         {
@@ -91,7 +90,6 @@ export default {
       this.loading = true
       this.apiCall().then((data) => {
         this.messages = data.data.content
-        console.log('MESSAGES ', this.messages)
         this.totalMessages = data.data.totalElements
         this.options.page = data.data.number
         this.loading = false
@@ -99,7 +97,7 @@ export default {
     },
     apiCall () {
       return axios.get(process.env.VUE_APP_SERVER_URL + '/api/atna/messages', {
-        params: { pageNumber: this.options.page, size: 10 }
+        params: { pageNumber: this.options.page - 1, size: this.itemPerPage }
       })
     }
   }
