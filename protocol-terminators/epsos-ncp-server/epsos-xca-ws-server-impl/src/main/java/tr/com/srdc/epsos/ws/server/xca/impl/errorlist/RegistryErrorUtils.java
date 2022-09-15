13b72ded5f8c4840d20c2091a54c659690178403
ghysmat <mathias.ghys@ext.ec.europa.eu>
2022-09-15 16:19:20 +0200

    [EHEALTH-8732] Revert RegistryErrorUtils to pass the OpenNCP and IHE error codes and not to use the generic XDSRegistryError error code.

--- protocol-terminators/epsos-ncp-server/epsos-xca-ws-server-impl/src/main/java/tr/com/srdc/epsos/ws/server/xca/impl/errorlist/RegistryErrorUtils.java
+++ protocol-terminators/epsos-ncp-server/epsos-xca-ws-server-impl/src/main/java/tr/com/srdc/epsos/ws/server/xca/impl/errorlist/RegistryErrorUtils.java
@@ -16,46 +16,53 @@ public class RegistryErrorUtils {
     private static final OMFactory omFactory = OMAbstractFactory.getOMFactory();
     private static final oasis.names.tc.ebxml_regrep.xsd.rs._3.ObjectFactory ofRs = new oasis.names.tc.ebxml_regrep.xsd.rs._3.ObjectFactory();
 
+    public static void addErrorMessage(RegistryErrorList registryErrorList, OpenNCPErrorCode openncpErrorCode, String codeContext, RegistryErrorSeverity severity) {
+        registryErrorList.getRegistryError().add(createErrorMessage(openncpErrorCode.getCode(), codeContext, null, severity));
+    }
+
     public static void addErrorMessage(RegistryErrorList registryErrorList, OpenNCPErrorCode openncpErrorCode, String codeContext, String location, RegistryErrorSeverity severity) {
-        registryErrorList.getRegistryError().add(createErrorMessage("[" + openncpErrorCode.getCode() + " - " + openncpErrorCode.getDescription() + "] " + codeContext, location, severity));
+        registryErrorList.getRegistryError().add(createErrorMessage(openncpErrorCode.getCode(), codeContext, location, severity));
     }
 
 
     public static void addErrorOMMessage(OMNamespace ons, OMElement registryErrorList, OpenNCPErrorCode openncpErrorCode, String codeContext, RegistryErrorSeverity severity) {
-        registryErrorList.addChild(createErrorOMMessage(ons, "[" + openncpErrorCode.getCode() + " - " + openncpErrorCode.getDescription() + "] " + codeContext, null, severity));
+        registryErrorList.addChild(createErrorOMMessage(ons, openncpErrorCode.getCode(), codeContext, null, severity));
     }
 
     public static void addErrorOMMessage(OMNamespace ons, OMElement registryErrorList, IheErrorCode iheErrorCode, String codeContext, RegistryErrorSeverity severity) {
-        registryErrorList.addChild(createErrorOMMessage(ons, "[" + iheErrorCode.getCode() + " - " + iheErrorCode.getDescription() + "] " + codeContext, null, severity));
+        registryErrorList.addChild(createErrorOMMessage(ons, iheErrorCode.getCode(), codeContext, null, severity));
     }
 
     public static void addErrorOMMessage(OMNamespace ons, OMElement registryErrorList, OpenNCPErrorCode openncpErrorCode, String codeContext, String location, RegistryErrorSeverity severity) {
-        registryErrorList.addChild(createErrorOMMessage(ons, "[" + openncpErrorCode.getCode() + " - " + openncpErrorCode.getDescription() + "] " + codeContext, location, severity));
+        registryErrorList.addChild(createErrorOMMessage(ons, openncpErrorCode.getCode(), codeContext, location, severity));
     }
 
     public static void addErrorOMMessage(OMNamespace ons, OMElement registryErrorList, ITMTSAMError error, String operationType, RegistryErrorSeverity severity) {
         registryErrorList.addChild(createErrorOMMessage(ons,
+                error.getCode(),
+                error.getDescription(),
                 "ECDATransformationHandler.Error." + operationType + "(" + error.getCode() + " / " + error.getDescription() + ")",
-                null,
                 severity));
     }
 
-    private static RegistryError createErrorMessage(String codeContext, String location, RegistryErrorSeverity severity) {
+    private static RegistryError createErrorMessage(String errorCode, String codeContext, String location, RegistryErrorSeverity severity) {
+
         var registryError = ofRs.createRegistryError();
-        registryError.setErrorCode(IheErrorCode.XDSRepositoryError.getCode());
+        registryError.setErrorCode(errorCode);
         registryError.setLocation(location);
         registryError.setSeverity(severity.getText());
         registryError.setCodeContext(codeContext);
         return registryError;
     }
 
-    private static OMElement createErrorOMMessage(OMNamespace ons, String codeContext, String location, RegistryErrorSeverity severity) {
+    private static OMElement createErrorOMMessage(OMNamespace ons, String errorCode, String codeContext, String location, RegistryErrorSeverity severity) {
 
         var registryError = omFactory.createOMElement("RegistryError", ons);
         registryError.addAttribute(omFactory.createOMAttribute("codeContext", null, codeContext));
-        registryError.addAttribute(omFactory.createOMAttribute("errorCode", null, IheErrorCode.XDSRepositoryError.getCode()));
-        String aux = severity != null? severity.getText() : null;
+        registryError.addAttribute(omFactory.createOMAttribute("errorCode", null, errorCode));
+        String aux = severity != null? severity.getText() : null ;
         registryError.addAttribute(omFactory.createOMAttribute("severity", null, aux));
+        // EHNCP-1131
         registryError.addAttribute(omFactory.createOMAttribute("location", null, location));
         return registryError;
     }
