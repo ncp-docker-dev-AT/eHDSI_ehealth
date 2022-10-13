53bbf58847f2d70f3b2396876cb3e49ecebadba0
alberto <albertomiori@hotmail.com>
2022-10-13 16:15:43 +0200

    EHEALTH-9165 - Pagination in eADC generate exception if startDate or endDate fields are empty

--- openncp-gateway/openncp-gateway-backend/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/module/eadc/persistence/model/EadcDbTimeConverter.java
+++ openncp-gateway/openncp-gateway-backend/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/module/eadc/persistence/model/EadcDbTimeConverter.java
@@ -1,5 +1,9 @@
 package eu.europa.ec.sante.ehdsi.openncp.gateway.module.eadc.persistence.model;
 
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.time.Instant;
@@ -7,9 +11,11 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Converter
 public class EadcDbTimeConverter implements AttributeConverter<Instant, String> {
+    private static final Logger logger = LoggerFactory.getLogger(EadcDbTimeConverter.class);
 
     private  final String DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss Z";
     private final ZoneId zoneId =  ZoneOffset.UTC; //ZoneId.systemDefault()
@@ -23,9 +29,21 @@ public class EadcDbTimeConverter implements AttributeConverter<Instant, String> 
 
     @Override
     public Instant convertToEntityAttribute(String s) {
+        if(StringUtils.isBlank(s)) {
+            logger.warn("date field is not populated!");
+            return Instant.EPOCH;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        ZonedDateTime timed = ZonedDateTime.parse(s, formatter);
-        timed.format(formatter);
-        return Instant.from(timed);
+        try {
+            ZonedDateTime timed = ZonedDateTime.parse(s, formatter);
+            timed.format(formatter);
+            return Instant.from(timed);
+        } catch (DateTimeParseException ex) {
+            logger.warn(ex.getMessage());
+            return Instant.EPOCH;
+        } catch (RuntimeException ex) {
+            logger.error(ex.getMessage());
+            throw (ex);
+        }
     }
 }
