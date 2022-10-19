0a40d3fdb12de2b974463b1778b7e0065415676e
miorial <alberto.miori@ext.ec.europa.eu>
2022-10-19 16:27:32 +0200

    EHEALTH-8778 - OpenNCP Gateway Backend - Spring Security warning

--- openncp-gateway/openncp-gateway-backend/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/config/WebSecurityConfiguration.java
+++ openncp-gateway/openncp-gateway-backend/src/main/java/eu/europa/ec/sante/ehdsi/openncp/gateway/config/WebSecurityConfiguration.java
@@ -8,12 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final TokenProvider tokenProvider;
