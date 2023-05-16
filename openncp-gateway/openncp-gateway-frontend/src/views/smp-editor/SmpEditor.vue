a5c4643f1493763898292632818899ef0dd97550
Jerome Subiger <jerome.subiger@ext.ec.europa.eu>
2023-05-16 19:31:08 +0200

    EHEALTH-9641 OpenNCP-Gateway: "SMP File Update" UI needs improvement

--- openncp-gateway/openncp-gateway-frontend/src/views/smp-editor/SmpEditor.vue
+++ openncp-gateway/openncp-gateway-frontend/src/views/smp-editor/SmpEditor.vue
@@ -3,7 +3,6 @@
     <span class="display-2">SMP Editor</span>
     <v-tabs v-model="selectedTab" optional>
       <v-tab :to="{ name: 'SmpGenerate' }">Generate</v-tab>
-      <v-tab :to="{ name: 'SmpUpdate' }" :disabled="true">Update</v-tab>
       <v-tab :to="{ name: 'SmpSign' }">Sign </v-tab>
       <v-tab :to="{ name: 'SmpUpload' }">Upload File to SMP Server</v-tab>
       <v-tab :to="{ name: 'SmpDelete' }">List SMP services</v-tab>
@@ -34,15 +33,13 @@ export default {
         const country = _.find(response.data, (item) => {
           return item.name === 'COUNTRY_CODE'
         }).value
-        // console.log(country)
         this.$store.commit('country', country)
         this.properties = response.data
         this.loading = false
       })
       .catch((err) => {
         this.error(
-          'An error occurs. The operations is not completed! <br/>' +
-            err.response.data.message
+          'An error occurs. The operations are not completed! <br/>' + err.response.data.message
         )
       })
   }
