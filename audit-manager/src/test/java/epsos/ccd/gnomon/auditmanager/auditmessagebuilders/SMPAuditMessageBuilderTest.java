7d2de027ab279e5a66948189fb554fa2142f6ce1
Mathias Ghys <mathias.ghys@ext.ec.europa.eu>
2024-02-16 16:06:23 +0100

    [EHEALTH-11061] Add unit test for SMPAuditMessageBuilder

--- audit-manager/src/test/java/epsos/ccd/gnomon/auditmanager/auditmessagebuilders/SMPAuditMessageBuilderTest.java
+++ audit-manager/src/test/java/epsos/ccd/gnomon/auditmanager/auditmessagebuilders/SMPAuditMessageBuilderTest.java
@@ -1,19 +1,54 @@
 package epsos.ccd.gnomon.auditmanager.auditmessagebuilders;
 
+import com.google.common.io.Resources;
 import epsos.ccd.gnomon.auditmanager.*;
 import eu.epsos.validation.datamodel.common.NcpSide;
 import net.RFC3881.AuditMessage;
+import org.custommonkey.xmlunit.XMLTestCase;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Assert;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 import tr.com.srdc.epsos.util.DateUtil;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.datatype.XMLGregorianCalendar;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
-public class SMPAuditMessageBuilderTest {
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+
+public class SMPAuditMessageBuilderTest extends XMLTestCase {
 
     @Test
-    public void testBuild() throws JAXBException {
+    public void testBuild() throws JAXBException, IOException, SAXException {
+
+        EventLog eventLog = new EventLog();
+        eventLog.setEventType(EventType.SMP_QUERY);
+        eventLog.setNcpSide(NcpSide.NCP_A);
+        eventLog.setEI_TransactionName(TransactionName.SMP_QUERY);
+        eventLog.setEI_EventActionCode(EventActionCode.EXECUTE);
+        eventLog.setEI_EventOutcomeIndicator(EventOutcomeIndicator.FULL_SUCCESS);
+        eventLog.setEI_EventDateTime(DateUtil.getDateAsXMLGregorian(new Date()));
+        eventLog.setSC_UserID("ncp-ppt.pt.ehealth.testa.eu");
+        eventLog.setSP_UserID("smp-test.publisher.ehealth.testa.eu");
+        eventLog.setAS_AuditSourceId("PT-1");
+        eventLog.setSourceip("10.200.137.161");
+        eventLog.setTargetip("smp-cert-auth-test.publisher.ehealth.testa.eu");
+        eventLog.setEM_ParticipantObjectDetail("errorMessage".getBytes());
+        SMPAuditMessageBuilder smpAuditMessageBuilder = new SMPAuditMessageBuilder();
+        AuditMessage auditMessage = smpAuditMessageBuilder.build(eventLog);
+        String generatedAuditMessage = AuditTrailUtils.convertAuditObjectToXML(auditMessage);
+        URL url = Resources.getResource("smpqueryauditmessage.xml");
+        String expectedAuditMessage = Resources.toString(url, StandardCharsets.UTF_8);
+        XMLUnit.setIgnoreWhitespace(true);
+        assertXMLEqual(expectedAuditMessage, generatedAuditMessage);
+    }
+
+    @Test
+    public void testBuildIncludingError() throws JAXBException {
 
         EventLog eventLog = new EventLog();
         eventLog.setEventType(EventType.SMP_QUERY);
